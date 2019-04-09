5af62f87ced17f5fdc94b1b12c32f758b4772934
dszendrei <r.daniel.szendrei@gmail.com>
2019-04-09 11:29:02 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5050));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
+
+app.get('/', function(req, res){
+    res.render('index.html');
+});
+
+app.listen(app.get('port'), function() {
+});
