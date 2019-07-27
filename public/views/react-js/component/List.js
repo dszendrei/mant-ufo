9fd27025bf07bcfd658baa18e303f14c7715ceef
dszendrei <r.daniel.szendrei@gmail.com>
2019-07-27 19:21:54 +0200

    Change fetch to heroku

--- public/views/react-js/component/List.js
+++ public/views/react-js/component/List.js
@@ -22,8 +22,8 @@ class List extends Component {
     }
 
     componentDidMount() {
-        fetch('http://localhost:8080/lists/' + this.state.worksheetName + '/' + this.state.range)
-        //fetch('https://mantufo-lists.herokuapp.com/lists/' + this.state.worksheetName + '/' + this.state.range)
+        //fetch('http://localhost:8080/lists/' + this.state.worksheetName + '/' + this.state.range)
+        fetch('https://mantufo-lists.herokuapp.com/lists/' + this.state.worksheetName + '/' + this.state.range)
             .then(response => response.json())
             .then(data => {
                 this.setState({ data })
